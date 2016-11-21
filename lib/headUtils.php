9dcc00b3c93c8d6f2c1461d68a54768ec6c8e547
Krieg Benally <kriegbenally@gmail.com>
2016-11-21 14:18:27 -0700

    created link for animated.css in headUtils.php

--- lib/headUtils.php
+++ lib/headUtils.php
@@ -1,3 +1,4 @@
 <link rel="stylesheet" href="lib/css/font-awesome.css">
+<link rel="stylesheet" href="lib/css/animate.css">
 <link rel="stylesheet" href="lib/css/bootstrap.css">
 <link rel="stylesheet" href="lib/css/style.css">
