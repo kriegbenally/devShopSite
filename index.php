adba5765804d279b974a102f28993daf75d39d8e
Krieg Benally <kriegbenally@gmail.com>
2016-11-21 14:12:42 -0700

    added everything.
    Added BS4 css, style.css, font-awesome.css, various js in order for BS4
    to function.  Added a partials directory for those php require_once.
    Everything is ran locally, there are no CDN's or external frameworks
    connecting outside of the our server.

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+     <?php require_once("lib/headUtils.php");?>
+  </head>
+  <body>
+
+
+    <?php require_once("lib/partials/scripts.php");?>
+
+  </body>
+</html>
---
+++ lib/headUtils.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="lib/css/font-awesome.css">
+<link rel="stylesheet" href="lib/css/bootstrap.css">
+<link rel="stylesheet" href="lib/css/style.css">
