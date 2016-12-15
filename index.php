8542ce4a4021bccee05c50fd67c25ea102645d45
Krieg Benally <kriegbenally@gmail.com>
2016-12-15 01:57:28 -0700

    added stuff

--- index.php
+++ index.php
@@ -6,8 +6,84 @@
      <?php require_once("lib/headUtils.php");?>
   </head>
   <body>
-      <?php require_once("lib/partials/navbar.php");?>
-      <?php require_once("lib/partials/carousel.php");?>
+    <?php require_once("lib/partials/navbar.php");?>
+    <?php require_once("lib/partials/carousel.php");?>
+
+    <!--  CARDS FOR PORTFOLIO SELFIES  -->
+
+    <div class="posts">
+      <div class="post">
+        <img src="lib/images/gjG2.png">
+        <div class="post-title">
+          <a href="#" class="toggle-info btn">
+            <span class="left"></span>
+            <span class="right"></span>
+          </a>
+          <h2>GJ Gordy<small>Ringleader</small></h2>
+        </div>
+        <div class="post-flap flap1">
+          <div class="post-description">
+            GJ has been know to wear "the pants" around the house.  Don't mess with her.  Lol
+          </div>
+          <div class="post-flap flap2">
+            <div class="post-actions">
+              <a href="#" class="btn">Read more</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    <!-- <div class="post">
+      <img src="http://s4c.cymru/temp/wave2.jpg">
+      <div class="post-title">
+        <a href="#" class="toggle-info btn">
+          <span class="left"></span>
+          <span class="right"></span>
+        </a>
+        <h2>
+            post title
+            <small>Image from unsplash.com</small>
+        </h2>
+      </div>
+      <div class="post-flap flap1">
+        <div class="post-description">
+          This grid is an attempt to make something nice that works on touch devices. Ignoring hover states when they're not available etc.
+        </div>
+        <div class="post-flap flap2">
+          <div class="post-actions">
+            <a href="#" class="btn">Read more</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="post">
+      <img src="http://s4c.cymru/temp/wave3.jpg">
+      <div class="post-title">
+        <a href="#" class="toggle-info btn">
+          <span class="left"></span>
+          <span class="right"></span>
+        </a>
+        <h2>
+            post title
+            <small>Image from unsplash.com</small>
+        </h2>
+      </div>
+      <div class="post-flap flap1">
+        <div class="post-description">
+          This grid is an attempt to make something nice that works on touch devices. Ignoring hover states when they're not available etc.
+        </div>
+        <div class="post-flap flap2">
+          <div class="post-actions">
+            <a href="#" class="btn">Read more</a>
+          </div>
+        </div>
+      </div>
+    </div> -->
+
+
+
+  </div>
 
     <?php require_once("lib/partials/scripts.php");?>
 
--- lib/partials/scripts.php
+++ lib/partials/scripts.php
@@ -2,3 +2,5 @@
 <script type="text/javascript" src="lib/js/tether.js"></script>
 <script type="text/javascript" src="lib/js/bootstrap.js"></script>
 <script type="text/javascript" src="lib/js/shiScripts.js"></script>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js'></script>
+<script type="text/javascript" src="lib/js/postCards.js"></script>
